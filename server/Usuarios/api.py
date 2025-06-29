4ec1d255b7f8476e9faad572df2ac878d070ace1
MiguelP04 <miguel09112004@gmail.com>
2025-06-29 09:44:20 -0400

    Haciendo el endpoint de buscar por cedula

--- server/Usuarios/api.py
+++
@@ -1,9 +0,0 @@
-from .models import Usuario
-from rest_framework import viewsets, permissions
-from Usuarios.serializers import UsuariosSerializer
-
-
-class UsuariosViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
-    serializer_class = UsuariosSerializer
--- server/Usuarios/views.py
+++ server/Usuarios/views.py
@@ -1,11 +1,24 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
 from .serializer import UsuariosSerializer
 from .models import Usuario
-
-
 
 # Create your views here.
 
 class UsuariosViewSet(viewsets.ModelViewSet):
     serializer_class = UsuariosSerializer
     queryset = Usuario.objects.all()
+
+    @action(detail=False, methods=['get'], url_path='buscar-por-cedula')
+    def buscar_por_cedula(self, request):
+        cedula = request.query_params.get('cedula')
+        if not cedula:
+            return Response({'error': "Falta el parámetro 'cedula'. "}, status=status.HTTP_400_BAD_REQUEST)
+        try: 
+            usuario = Usuario.objects.get(cedula=cedula)
+            serializer = self.get_serializer(usuario)
+            return Response(serializer.data)
+        except Usuario.DoesNotExist:
+            return Response({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
