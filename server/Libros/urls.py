c4a716bc133fae0f8515e7908d9056696c2e5695
MiguelP04 <miguel09112004@gmail.com>
2025-06-30 11:47:40 -0400

    Arreglando el endpoint de categorias

--- server/Libros/urls.py
+++ server/Libros/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import LibroViewSet, CategoriaViewSet
 
 router = DefaultRouter()
-router.register(r'', LibroViewSet, basename='libro')
 router.register(r'categorias', CategoriaViewSet, basename='categoria')
+router.register(r'', LibroViewSet, basename='libro')
 
 urlpatterns = router.urls
