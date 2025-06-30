8971deee1e648cb3b0e6ad79dc9b48c2957802e6
MiguelP04 <miguel09112004@gmail.com>
2025-06-30 11:27:19 -0400

    Arreglando el endpoint de libros

--- server/Libros/urls.py
+++ server/Libros/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import LibroViewSet, CategoriaViewSet
 
 router = DefaultRouter()
-router.register(r'libros', LibroViewSet, basename='libro')
+router.register(r'', LibroViewSet, basename='libro')
 router.register(r'categorias', CategoriaViewSet, basename='categoria')
 
 urlpatterns = router.urls
